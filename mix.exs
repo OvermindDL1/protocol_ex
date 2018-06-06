aed71c8c1ec3bd4b2fb3b33442f4fc67fc6453c9
OvermindDL1 <overminddl1@gmail.com>
2018-06-06 08:54:40 -0600

    Tests passed, bumping version and publish

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ProtocolEx.Mixfile do
   def project do
     [
       app: :protocol_ex,
-      version: "0.3.19",
+      version: "0.3.20",
       elixir: "~> 1.4",
       description: description(),
       package: package(),
